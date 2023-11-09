6e06f3152c0ad218ed3c5c17a99a0a0bdab67842
AkashChintaluri <akashchintaluriofficial@gmail.com>
2023-11-09 14:41:56 +0530

    SEcond COmmit

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,75 +1,100 @@
-class TreeNode:
+class Node:
     def __init__(self, data):
         self.data = data
-        self.left = None  # For "yes" answers
-        self.right = None  # For "no" answers
+        self.yes_link = None
+        self.no_link = None
 
-def play_guessing_game(node):
-    while node:
-        if node.left and node.right:
-            print(node.data)
-            user_input = input("Enter 'yes' or 'no': ").strip().lower()
+class BinaryTree:
+    def __init__(self):
+        self.root = None
 
-            if user_input == 'yes':
-                node = node.left
-            elif user_input == 'no':
-                node = node.right
-        elif node.left:
-            print(node.data)
-            user_input = input("Enter 'yes' or 'no': ").strip().lower()
-            if user_input == 'yes':
-                node = node.left
-            else:
-                print("Great! I guessed it.")
-                return node.data  # Return the correctly guessed result
+    def build_tree(self, game_file):
+        with open(game_file, 'r') as file:
+            self.root = self._build_tree_recursive(file)
+
+    def _build_tree_recursive(self, file):
+        line = file.readline().strip()
+        if not line:
+            return None
+
+        data = line.split()
+        node = Node(data[1])
+
+        if data[0].isdigit():
+            node.yes_link = self._build_tree_recursive(file)
+            node.no_link = self._build_tree_recursive(file)
+
+        return node
+
+    def display_tree(self, order):
+        if order == '1':
+            self._inorder(self.root)
+        elif order == '2':
+            self._preorder(self.root)
+        elif order == '3':
+            self._postorder(self.root)
         else:
-            print(f"Great! I guessed it.")
-            return node.data  # Return the correctly guessed result
+            print("Invalid order choice")
 
-# Create the hierarchical tree structure
-root = TreeNode("Is it a living thing?")
-root.left = TreeNode("Is it an animal?")
-root.right = TreeNode("Is it a non-living thing?")
+    def _inorder(self, node):
+        if node:
+            self._inorder(node.yes_link)
+            print(node.data)
+            self._inorder(node.no_link)
 
-# Living Things
-root.left.left = TreeNode("Is it a mammal?")
-root.left.right = TreeNode("Is it a reptile?")
-root.left.left.left = TreeNode("Is it a carnivore?")
-root.left.left.right = TreeNode("Is it a herbivore?")
-root.left.right.left = TreeNode("Is it a snake?")
-root.left.right.right = TreeNode("Is it a bird?")
-root.left.left.left.left = TreeNode("Lion")
-root.left.left.left.right = TreeNode("Tiger")
-root.left.left.right.left = TreeNode("Python")
-root.left.left.right.right = TreeNode("Rattlesnake")
-root.left.right.left.left = TreeNode("Crocodile")
-root.left.right.left.right = TreeNode("Snake")
-root.left.right.right.left = TreeNode("Eagle")
-root.left.right.right.right = TreeNode("Owl")
+    def _preorder(self, node):
+        if node:
+            print(node.data)
+            self._preorder(node.yes_link)
+            self._preorder(node.no_link)
 
-# Non-Living Things
-root.right.left = TreeNode("Is it man-made?")
-root.right.right = TreeNode("Is it found in nature?")
-root.right.left.left = TreeNode("Is it a vehicle?")
-root.right.left.right = TreeNode("Is it an electronic device?")
-root.right.left.left.left = TreeNode("Car")
-root.right.left.left.right = TreeNode("Bicycle")
-root.right.left.right.left = TreeNode("Smartphone")
-root.right.left.right.right = TreeNode("Laptop")
-root.right.right.left = TreeNode("Is it a mineral?")
-root.right.right.right = TreeNode("Is it a rock?")
-root.right.right.left.left = TreeNode("Gold")
-root.right.right.left.right = TreeNode("Diamond")
-root.right.right.right.left = TreeNode("Granite")
-root.right.right.right.right = TreeNode("Marble")
+    def _postorder(self, node):
+        if node:
+            self._postorder(node.yes_link)
+            self._postorder(node.no_link)
+            print(node.data)
 
-# Abstract Concepts
-root.right.right.right.right = TreeNode("Sun")
-root.right.right.right.left = TreeNode("Moon")
-root.right.right.right.left.left = TreeNode("Time")
-root.right.right.right.left.right = TreeNode("Love")
+    def play_game(self):
+        current_node = self.root
+        while True:
+            print(current_node.data)
+            if not current_node.yes_link and not current_node.no_link:
+                break
 
-# Start the game from the root node
-correct_guess = play_guessing_game(root)
-print(f"The correctly guessed result is: {correct_guess}")
+            answer = input("Enter your answer (Y/N): ").strip().lower()
+            while answer not in ['y', 'n', 'yes', 'no']:
+                print("Invalid input. Please enter 'Y' or 'N'.")
+                answer = input("Enter your answer (Y/N): ").strip().lower()
+
+            if answer in ['y', 'yes']:
+                current_node = current_node.yes_link
+            else:
+                current_node = current_node.no_link
+
+if __name__ == "__main__":
+    binary_tree = BinaryTree()
+
+    # Load the default game file
+    binary_tree.build_tree("game1.txt")
+
+    while True:
+        print("\nMovie Guessing Game")
+        print("P Play the game")
+        print("L Load another game file")
+        print("D Display the binary tree")
+        print("X Exit the program")
+        choice = input("...your choice: ").strip().lower()
+
+        if choice == "p":
+            binary_tree.play_game()
+        elif choice == "l":
+            file_name = input("Enter the file name to load: ")
+            binary_tree.build_tree(file_name)
+        elif choice == "d":
+            order = input("What order do you want to display?\n1. Inorder\n2. Preorder\n3. Postorder\n... your choice: ")
+            binary_tree.display_tree(order)
+        elif choice == "x":
+            break
+        else:
+            print("Invalid option. Please choose a valid option.")
 
